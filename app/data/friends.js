acb223cda4176c9f97986a62fca2f6ea5ccb5dd3
sharonpamela <sharon.pamela.santana@gmail.com>
2019-04-01 23:57:54 -0700

    basic app config setup

---
+++ app/data/friends.js
@@ -0,0 +1,21 @@
+
+let friendsData = [
+    {
+        "name":"Lisa Spears",
+        "photo":"https://images.pexels.com/photos/356378/pexels-photo-356378.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+        "scores":[
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+          ]
+      }
+]
+
+module.exports = friendsData;
--- server.js
+++ server.js
@@ -1,1 +1,22 @@
+// Dependencies 
 let express = require("express");
+let path = require("path");
+
+// Tells node that we are creating an "express" server
+let app = express();
+
+// Sets an initial port.
+let PORT = process.env.PORT || 8080;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Router
+require("./routing/apiRoutes")(app);
+require("./routing/htmlRoutes")(app);
+
+// Listener
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
+});
