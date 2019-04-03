ff400650c7c9ff51355d7906c6a6b4e98f067fca
sharonpamela <sharon.pamela.santana@gmail.com>
2019-04-02 21:27:13 -0700

    init config

--- server.js
+++ server.js
@@ -6,15 +6,15 @@ let path = require("path");
 let app = express();
 
 // Sets an initial port.
-let PORT = process.env.PORT || 8080;
+let PORT = process.env.PORT || 3000;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Router
-require("./routing/apiRoutes")(app);
-require("./routing/htmlRoutes")(app);
+require("./app/routing/apiRoutes.js")(app);
+require("./app/routing/htmlRoutes.js")(app);
 
 // Listener
 app.listen(PORT, function() {
